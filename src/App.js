cca7b05fc5343e0e0e08002458e8d19e94710ab4
mdmeeran90 <meeranfreek2193@gmail.com>
2024-12-17 13:19:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './componets/Home';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+       <h1>Yusuf Afsal Ahamed Jamal majith jamal meeran jamal afsal jamal</h1>
+       
+      </header>
+      <Home />
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ src/componets/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './home.css'
+
+export default function Home() {
+    return (
+        <div>
+            <h2>This Is  Member of Good Friendship</h2>
+            <p> this concept was they element of they stroage was this is friendships
+            </p>
+        </div>
+    )
+}
+
+                                                                                                                                
